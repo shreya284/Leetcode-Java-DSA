64862a6d2d64f54211f37308516284f5a9d1dd3b
Shreya Subhash Shetty <71548879+shreya284@users.noreply.github.com>
2024-08-30 20:09:39 +0530

    Create ValidateBinarySearchTree.java

---
+++ Advanced-DSA/Binary Tree/ValidateBinarySearchTree.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return valid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    }
+    
+    boolean valid(TreeNode node,long start,long end)
+    {
+        if(node == null)
+        {
+            return true;
+        }
+        
+        if(start < node.val && node.val < end)
+        {
+            return valid(node.left,start,node.val) && valid(node.right,node.val,end);
+        }
+        return false;
+    }
+}
